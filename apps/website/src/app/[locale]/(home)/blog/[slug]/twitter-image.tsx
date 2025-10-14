f56059c215a2febae2a431cdd98dc8c21f072b2c
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-10-14 13:26:02 -0700

    refactor: simplify x og (#604)

--- apps/website/src/app/[locale]/(home)/blog/[slug]/twitter-image.tsx
+++ apps/website/src/app/[locale]/(home)/blog/[slug]/twitter-image.tsx
@@ -1,5 +1,1 @@
-import Image, { alt as ogAlt, size as ogSize } from './opengraph-image'
-
-export const alt = ogAlt
-export const size = ogSize
-export default Image
+export { alt, default, size } from './opengraph-image'
