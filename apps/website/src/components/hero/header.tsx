0d4d3c5ff635f31d83f18cfde4df4a8a3ef3f740
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-10-16 20:27:51 -0700

    chore: update hero heading (#619)

--- apps/website/src/components/hero/header.tsx
+++ apps/website/src/components/hero/header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { IconLanguage, IconMoustache } from '@tabler/icons-react'
 import { motion } from 'motion/react'
 import { useTranslations } from 'next-intl'
 import React from 'react'
@@ -21,12 +22,18 @@ export function Header() {
         viewport={{ once: true }}
         className="w-full md:max-w-6xl flex flex-col gap-4 items-center justify-center px-8 md:px-4 pt-20"
       >
-        <h2 className="w-full text-balance whitespace-pre-wrap text-4xl md:text-7xl font-bold dark:text-white text-center tracking-normal">
-          {t('titleBefore')}
-          <span className="text-primary-strong mx-2 break-words">
-            {t('study')}
+        <h2 className="w-full text-balance whitespace-pre-wrap text-4xl md:text-7xl font-bold dark:text-white text-center tracking-normal leading-11 md:leading-20">
+          {t('yourLanguage')}
+          {' '}
+          <span className="inline-block align-middle">
+            <IconLanguage className="w-12 h-12 md:w-20 md:h-20 text-blue-500 inline-block align-middle" stroke={1.5} />
+            {t('translator')}
           </span>
-          {t('titleAfter')}
+          {' '}
+          <span className="inline-block align-middle">
+            <IconMoustache className="w-12 h-12 md:w-20 md:h-20 text-amber-500 inline-block align-middle" stroke={1.5} />
+            {t('teacher')}
+          </span>
         </h2>
         <div className="max-w-4xl text-base text-neutral-600 dark:text-neutral-200 py-4 text-center">
           {t('subtitle')}
